f7ab35726641f0e183810b358e21c666b053b8a4
Roman Štěpánek <55704853+SteproCZ@users.noreply.github.com>
2019-10-14 13:52:42 +0200

    Add files via upload

---
+++ INPTPZ19-1/ComplexNumber.cs
@@ -0,0 +1,60 @@
+﻿namespace INPTPZ1
+{
+    class ComplexNumber
+    {
+        public double Re { get; set; }
+        public double Im { get; set; }
+
+        public readonly static ComplexNumber Zero = new ComplexNumber()
+        {
+            Re = 0,
+            Im = 0
+        };
+
+        public ComplexNumber Multiply(ComplexNumber complexNumber)
+        {
+            return new ComplexNumber()
+            {
+                Re = this.Re * complexNumber.Re - this.Im * complexNumber.Im,
+                Im = this.Re * complexNumber.Im + this.Im * complexNumber.Re
+            };
+        }
+
+        public ComplexNumber Add(ComplexNumber complexNumber)
+        {
+            return new ComplexNumber()
+            {
+                Re = this.Re + complexNumber.Re,
+                Im = this.Im + complexNumber.Im
+            };
+        }
+        public ComplexNumber Subtract(ComplexNumber complexNumber)
+        {
+            return new ComplexNumber()
+            {
+                Re = this.Re - complexNumber.Re,
+                Im = this.Im - complexNumber.Im
+            };
+        }
+
+        public override string ToString()
+        {
+            return $"({Re} + {Im}i)";
+        }
+
+        internal ComplexNumber Divide(ComplexNumber complexNumber)
+        {
+            ComplexNumber dividedComplexNumber = this.Multiply(
+                new ComplexNumber() {
+                    Re = complexNumber.Re,
+                    Im = -complexNumber.Im });
+            double divisor = complexNumber.Re * complexNumber.Re + complexNumber.Im * complexNumber.Im;
+
+            return new ComplexNumber()
+            {
+                Re = dividedComplexNumber.Re / divisor,
+                Im = dividedComplexNumber.Im / divisor
+            };
+        }
+    }
+}
---
+++ INPTPZ19-1/Polynome.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+
+
+namespace INPTPZ1
+{
+    class Polynome
+    {
+        public List<ComplexNumber> Coeficients { get; set; }
+
+        public Polynome()
+        {
+            Coeficients = new List<ComplexNumber>();
+        }
+
+        public Polynome Derive()
+        {
+            Polynome polynome = new Polynome();
+            for (int i = 1; i < Coeficients.Count; i++)
+            {
+                polynome.Coeficients.Add(Coeficients[i].Multiply(new ComplexNumber() { Re = i }));
+            }
+
+            return polynome;
+        }
+
+        public ComplexNumber Eval(ComplexNumber complexNumber)
+        {
+            ComplexNumber zeroComplexNumber = ComplexNumber.Zero;
+            for (int i = 0; i < Coeficients.Count; i++)
+            {
+                ComplexNumber coeficientComplexNumber = Coeficients[i];
+                ComplexNumber copyComplexNumber = complexNumber;
+                int power = i;
+
+                if (i > 0)
+                {
+                    for (int j = 0; j < power - 1; j++)
+                    {
+                        copyComplexNumber = copyComplexNumber.Multiply(complexNumber);
+                    }                        
+
+                    coeficientComplexNumber = coeficientComplexNumber.Multiply(copyComplexNumber);
+                }
+
+                zeroComplexNumber = zeroComplexNumber.Add(coeficientComplexNumber);
+            }
+
+            return zeroComplexNumber;
+        }
+
+        public override string ToString()
+        {
+            string resultString = string.Empty;
+            for (int i = 0; i < Coeficients.Count; i++)
+            {
+                resultString += Coeficients[i];
+                if (i > 0)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        resultString += 'x';
+                    }
+                }
+                resultString += " + ";
+            }
+            return resultString;
+        }
+    }
+}
